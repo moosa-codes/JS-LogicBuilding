14b6d219b46ec1be26ef9f5350577146ee605c42
Moosa Haroon <157456649+MoosaHaroon6@users.noreply.github.com>
2025-01-18 00:50:15 +0500

    Default parameters in js + example

---
+++ Default-parameter/index.js
@@ -0,0 +1,36 @@
+/*
+  Q) What is Default parameter/argument in js ?
+     
+  ** A default parameter in JavaScript is a value 
+    you set for a function's parameter, it runs when no
+    value is provided when the function is called.
+    The following are examples :
+*/
+
+
+
+// simple example 
+function simpleCalculation(x, y = 2) {
+    const result = x + y;
+    return result;
+}
+
+console.log(simpleCalculation(10)); // output : 12 => 10 + 2;
+console.log(simpleCalculation(10, 20)); // output : 30 => 10 + 20; 
+
+
+// complex example 
+function calculateTotalCost(price, tax = (5 / 100), discount = 0, shipping = 10) {
+    let taxAmount = price * (tax / 100);
+    console.log("Tax", taxAmount);
+
+    let discountAmount = price * (discount / 100);
+    console.log("Discounted", discountAmount);
+
+    let finalCost = price + taxAmount - discountAmount;
+    console.log("Final Cost", finalCost + shipping + "$");
+}
+
+calculateTotalCost(100); // 110.05$
+console.log(" ");
+calculateTotalCost(1000, 10, 60, 30); // 530$
